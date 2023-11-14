802b2acdfeb3487efe050bdb97f2240f427bad65
marwood96 <2207184@leedstrinity.ac.uk>
2023-11-14 17:59:50 +0000

    Updated Updated

--- Untitled-3.js
+++ Untitled-3.js
@@ -1,3 +1,8 @@
-if(document.getElementById("mobilemenu").style.display == "block") {
-    document.getElementById("navbarlist").style.display = "none";
-}
+document.getElementById('hamburger').addEventListener('click', function() {
+    var menu = document.getElementById('hamburgerlist');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
